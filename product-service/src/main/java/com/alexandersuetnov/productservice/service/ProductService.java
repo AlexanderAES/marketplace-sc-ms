63bc5913a351ac716918c45de891d2b2ba294412
Alexander Suetnov <aes123.aa@gmail.com>
2022-06-04 18:33:04 +0300

    add methods into ProductService: getAllProductsByTitle, deleteProduct, updateProduct

--- product-service/src/main/java/com/alexandersuetnov/productservice/service/ProductService.java
+++ product-service/src/main/java/com/alexandersuetnov/productservice/service/ProductService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+import java.security.Principal;
 import java.util.List;
 
 @Service
@@ -18,7 +19,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public Product createProduct(ProductDTO productDTO) {
+    public Product createProduct(ProductDTO productDTO, Principal principal) {
             Product product = ProductMapper.INSTANCE.ProductDTOtoProduct(productDTO);
             log.info("Saving product for user with id: {}", product.getUserId());
             return productRepository.save(product);
@@ -28,8 +29,8 @@ public class ProductService {
         return productRepository.findById(productId).orElseThrow(() -> new ProductNotFoundException(productId));
     }
 
-    // возвращает все объявления из бд удовлетворяющие поисковому слову
-    public List<Product> getAllProducts(String title) {
+
+    public List<Product> getAllProductsByTitle(String title) {
         if (title != null)
             return productRepository.findByTitleContainingIgnoreCase(title);
         return productRepository.findAll();
@@ -38,5 +39,31 @@ public class ProductService {
     public List<Product> getAllProduct() {
         return productRepository.findAll();
     }
+
+    public String deleteProduct(String productId) {
+        String responseMessage = String.format("Deleting product with id %s ", productId);
+        productRepository.deleteById(Long.parseLong(productId));
+        log.info("Deleting product with id: {}", productId);
+        return responseMessage;
+    }
+
+    public String updateProduct(ProductDTO productDTO,String userId,String productId) {
+        Product currentProduct = getProductById(Long.parseLong(productId));
+        String responseMessage;
+        if (productDTO != null){
+
+            //ToDo сделать проверку на соответствие продукта и пользователя isEquals(){}
+
+            currentProduct = ProductMapper.INSTANCE.ProductDTOtoProduct(productDTO);
+            currentProduct.setUserId(userId);
+            responseMessage = String.format("Update product with title {}", productDTO.getTitle());
+            log.info("Update product with title {}", productDTO.getTitle());
+            return responseMessage;
+        }
+        return null;
+    }
+
+//    Todo добавить метод "boolean isEquals(){}"
+
 
 }
