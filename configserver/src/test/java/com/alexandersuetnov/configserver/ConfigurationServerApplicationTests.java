8674642d8a41a7720f40a60b319010b080007410
Alexander Suetnov <aes123.aa@gmail.com>
2022-06-06 11:59:10 +0300

    add ConfigServer

--- configserver/src/test/java/com/alexandersuetnov/configserver/ConfigurationServerApplicationTests.java
+++ configserver/src/test/java/com/alexandersuetnov/configserver/ConfigurationServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.alexandersuetnov.marketplacescms;
+package com.alexandersuetnov.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MarketplaceScMsApplicationTests {
+class ConfigurationServerApplicationTests {
 
 	@Test
 	void contextLoads() {
