34d23130f5947eef5e0d2e43383d4a07582ef9d8
Alexander Suetnov <aes123.aa@gmail.com>
2022-06-16 19:16:21 +0300

    add LoginRequest, InvalidLoginResponse

---
+++ user-service/src/main/java/com/alexandersuetnov/userserviceapp/payload/response/InvalidLoginResponse.java
@@ -0,0 +1,14 @@
+package com.alexandersuetnov.userserviceapp.payload.response;
+
+import lombok.Getter;
+
+@Getter
+public class InvalidLoginResponse {
+    private String username;
+    private String password;
+
+    public InvalidLoginResponse() {
+        this.username = "Invalid Username";
+        this.password = "Invalid Password";
+    }
+}
