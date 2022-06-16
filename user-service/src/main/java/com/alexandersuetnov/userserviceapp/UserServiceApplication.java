f94549671cd4745d6a9949a198e9185b793f1ac1
Alexander Suetnov <aes123.aa@gmail.com>
2022-06-16 19:22:24 +0300

    add UserServiceApplication, ProductServiceApplication

--- user-service/src/main/java/com/alexandersuetnov/userserviceapp/UserServiceApplication.java
+++ user-service/src/main/java/com/alexandersuetnov/userserviceapp/UserServiceApplication.java
@@ -1,15 +1,21 @@
-package com.alexandersuetnov.productservice;
+package com.alexandersuetnov.userserviceapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+
 @SpringBootApplication
-public class ProductServiceApplication {
+@RefreshScope
+@EnableEurekaClient
+public class UserServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ProductServiceApplication.class, args);
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 
     @Bean
