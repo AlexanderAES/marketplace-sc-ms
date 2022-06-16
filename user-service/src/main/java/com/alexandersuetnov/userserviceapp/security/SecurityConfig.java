08a242e197be86a16c521bb9bce57dd072f9e9fd
Alexander Suetnov <aes123.aa@gmail.com>
2022-06-16 19:15:41 +0300

    add Security

---
+++ user-service/src/main/java/com/alexandersuetnov/userserviceapp/security/SecurityConfig.java
@@ -0,0 +1,70 @@
+package com.alexandersuetnov.userserviceapp.security;
+
+import com.alexandersuetnov.userserviceapp.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static com.alexandersuetnov.userserviceapp.security.SecurityConstants.*;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests().antMatchers(SecurityConstants.ADMIN_PANEL).hasAnyAuthority("ROLE_ADMIN")
+                .antMatchers(SecurityConstants.SIGN_UP_URLS,
+                        GET_ALL_PRODUCTS, CONFIRM_REG,
+                        API_DOCS, SWAGGER_UI, SWAGGER_UI_HTML)
+                .permitAll()
+                .anyRequest().authenticated();
+
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    }
+
+    @Override
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JWTAuthenticationFilter jwtAuthenticationFilter() {
+        return new JWTAuthenticationFilter();
+    }
+}
---
+++ user-service/src/main/java/com/alexandersuetnov/userserviceapp/security/SecurityConstants.java
@@ -0,0 +1,16 @@
+package com.alexandersuetnov.userserviceapp.security;
+
+public class SecurityConstants {
+    public static final String SIGN_UP_URLS = "/users/*";
+    public static final String ADMIN_PANEL = "api/v1/admin/**";
+    public static final String GET_ALL_PRODUCTS = "api/v1/product/all";
+    public static final String CONFIRM_REG = "/users/activate/**";
+    public static final String API_DOCS = "/v3/api-docs/**";
+    public static final String SWAGGER_UI = "/swagger-ui/**";
+    public static final String SWAGGER_UI_HTML = "/swagger-ui.html";
+    public static final String SECRET = "SecretKeyGenJWT";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String CONTENT_TYPE = "application/json";
+    public static final long EXPIRATION_TIME = 1200_000; //20min
+}
