4c9764a82df2f9adec4fc46d2cc6aa5f6f5111ed
Alexander Suetnov <aes123.aa@gmail.com>
2022-06-20 15:47:51 +0300

    add SecurityConstants to productservice for validation tokens

---
+++ product-service/src/main/java/com/alexandersuetnov/productservice/config/SecurityConstants.java
@@ -0,0 +1,10 @@
+package com.alexandersuetnov.productservice.config;
+
+public class SecurityConstants {
+
+    public static final String SECRET = "SecretKeyGenJWTSecretKeyGenJWTSecretKeyGenJWT";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final long EXPIRATION_TIME = 1200_000; //20min
+
+}
