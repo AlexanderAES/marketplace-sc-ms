34a3a048655d70f6963df2864a4e85a0026b256a
Alexander Suetnov <aes123.aa@gmail.com>
2022-06-20 17:28:35 +0300

    add Image model

---
+++ image-service/src/main/java/com/alexandersuetnov/imageservice/model/Image.java
@@ -0,0 +1,52 @@
+package com.alexandersuetnov.imageservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "images")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "originalFileName")
+    private String originalFileName;
+
+    @Column(name = "size")
+    private Long size;
+
+    @Column(name = "contentType")
+    private String contentType;
+
+    @Column(name = "isPreviewImage")
+    private boolean isPreviewImage;
+
+    @Lob
+    @Column(columnDefinition = "LONGBLOB")
+    private byte[] bytes;
+
+    @Column(name = "user_id")
+    private String userId;
+
+    @Column(name = "create_date")
+    private LocalDateTime createDate;
+
+    @PrePersist
+    protected void init() {
+        createDate = LocalDateTime.now();
+    }
+
+}
