745911191ead722a414c6c8815f569750a01a9fe
Alexander Suetnov <aes123.aa@gmail.com>
2022-06-20 15:30:38 +0300

    add RouterValidator

---
+++ gateway-service/src/main/java/alexandersuetnov/gateway/config/RouterValidator.java
@@ -0,0 +1,27 @@
+package alexandersuetnov.gateway.config;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class RouterValidator {
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/users/signin",
+            "/users/signup",
+            "/activate",
+            "/users/info",
+            "/products/all",
+            "/products/info"
+
+    );
+
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+
+}
