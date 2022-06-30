5b90d53562afbba764f59490f3b7a654d57ef985
Alexander Suetnov <aes123.aa@gmail.com>
2022-06-30 10:52:38 +0300

    add annotation EnableEurekaClient

--- image-service/src/main/java/com/alexandersuetnov/imageservice/ImageServiceApplication.java
+++ image-service/src/main/java/com/alexandersuetnov/imageservice/ImageServiceApplication.java
@@ -1,9 +1,11 @@
-package alexandersuetnov.imageservice;
+package com.alexandersuetnov.imageservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ImageServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ImageServiceApplication.class, args);
